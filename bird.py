c3e2e8d85463169d8f85e42ecd4d1cd1fd2357b3
choi minju <chlminju32@gmail.com>
2025-11-04 20:52:13 +0900

    Initial commit

---
+++ bird.py
@@ -0,0 +1,22 @@
+from pico2d import load_image, get_time, load_font
+
+import game_world
+import game_framework
+from ball import Ball
+from state_machine import StateMachine
+
+
+class bird:
+    def __init__(self):
+        pass
+
+    def update(self):
+        pass
+
+
+    def handle_event(self, event):
+        pass
+
+    def draw(self):
+        pass
+
